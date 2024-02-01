d51d08fe35b65e3ab13a8b469ba91e92c4c09f1e
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-02-01 15:51:49 +0100

    inseriti come card

--- resources/views/admin/movie/index.blade.php
+++ resources/views/admin/movie/index.blade.php
@@ -4,14 +4,36 @@
     </h2>
 
     <ul>
-        @foreach ($movies as $movie )
-
-            <li>
-                {{ $movie -> title }}
-            </li>
-            
-        @endforeach
+       
     </ul>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="row row-cols-md-3 row-cols-xl-4 g-3">
+                    <div class="col">
+                        @foreach ($movies as $movie )
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3>
+                                        {{ $movie -> title }}
+                                    </h3>
+                
+                                </div>
+                
+                            </div>
+                        
+                        
+                        @endforeach
+                        
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
 
 @section('main-content')
 
